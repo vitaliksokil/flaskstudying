ae65e8a69cedcdaf8788238cb144c43132c20157
vitaliksokil <vitaliksokil200@gmail.com>
2020-10-28 23:46:31 +0200

    lab2

---
+++ run.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+
+"""
+hello_flask: First Python-Flask webapp
+"""
+from app import app
+if __name__ == '__main__':  # Script executed directly?
+    app.run()  # Launch built-in web server and run this Flask webapp
